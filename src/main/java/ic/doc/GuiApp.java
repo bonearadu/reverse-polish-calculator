0164ff74af58992d0e0cb069334edbad7b13e17f
Radu Bonea <bonearadu@gmail.com>
2020-11-16 17:32:58 +0200

    Implement back end.
    Contributors: [@rab19][@rf419]

--- src/main/java/ic/doc/GuiApp.java
+++ src/main/java/ic/doc/GuiApp.java
@@ -9,7 +9,6 @@ public class GuiApp {
   }
 
   private void display() {
-
     JFrame frame = new JFrame("Reverse Polish Calculator");
     frame.setSize(400, 300);
 
@@ -17,19 +16,28 @@ public class GuiApp {
     JTextField textField = new JTextField("0");
     textField.setColumns(10);
 
+    BackEnd backend = new BackEnd();
+
     for (int i = 1; i <= 4; i++) {
       JButton button = new JButton(Integer.toString(i));
       panel.add(button);
       int finalI = i;
       button.addActionListener(
-          e -> textField.setText(Integer.toString(finalI)));
+          e -> {
+            backend.pushToStack(finalI);
+            textField.setText(Integer.toString(finalI));
+          });
     }
 
     JButton plus = new JButton("+");
     panel.add(plus);
+    plus.addActionListener(e ->
+        textField.setText(Integer.toString(backend.executeOperation('+'))));
 
     JButton sub = new JButton("-");
     panel.add(sub);
+    sub.addActionListener(e ->
+        textField.setText(Integer.toString(backend.executeOperation('-'))));
 
     panel.add(textField);
 
